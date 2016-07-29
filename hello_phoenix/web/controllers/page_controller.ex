e355cf3c11e067c56ea6cad068cf5e7caa5a40f3
H.Tajima <tajihiro2@gmail.com>
2016-07-30 00:15:47 +0900

    Commit phoenix_sample Project.

--- hello_phoenix/web/controllers/page_controller.ex
+++
@@ -1,9 +0,0 @@
-defmodule HelloPhoenix.PageController do
-  use HelloPhoenix.Web, :controller
-
-  plug :action
-
-  def index(conn, _params) do
-    render conn, "index.html"
-  end
-end
---
+++ phoenix_sample/web/views/hello_view.ex
@@ -0,0 +1,3 @@
+defmodule PhoenixSample.HelloView do
+  use PhoenixSample.Web, :view
+end
